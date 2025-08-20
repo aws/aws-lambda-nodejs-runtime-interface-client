d731168543f0ff112684de0092e39499ffce64a2
Fabiana Severin <fabisev@amazon.com>
2025-08-20 21:34:20 +0100

    Adding specific error message for different node versions and formatiing

--- scripts/test_local.sh
+++ scripts/test_local.sh
@@ -23,29 +23,35 @@ fi
 
 # Function to run unit tests for specific Node version
 run_unit_tests() {
-local node_version=$1
-echo "Running unit tests for Node.js $node_version..."
-docker build -f test/unit/Dockerfile.nodejs${node_version}.x -t unit/nodejs.${node_version}x .
-docker run --rm unit/nodejs.${node_version}x
+    local node_version=$1
+    echo "Running unit tests for Node.js $node_version..."
+    docker build -f test/unit/Dockerfile.nodejs${node_version}.x -t unit/nodejs.${node_version}x . || {
+        echo "Build failed for Node.js $node_version"
+        exit 1
+    }
+    docker run --rm unit/nodejs.${node_version}x || {
+        echo "Tests failed for Node.js $node_version"
+        exit 1
+    }
 }
 
 # Parse command line arguments
 case "${1:-all}" in
-"unit")
-NODE_VERSION=${2:-"20"}
-run_unit_tests $NODE_VERSION
-;;
-"all")
-echo "Running unit tests for all Node versions..."
-for version in 18 20 22; do
-run_unit_tests $version
-done
-;;
-*)
-echo "Usage: $0 [unit|all] [node_version]"
-echo "Examples:"
-echo " $0 unit 20 # Run unit tests for Node 20"
-echo " $0 all # Run unit tests for all Node versions"
-exit 1
-;;
+    "unit")
+        NODE_VERSION=${2:-"20"}
+        run_unit_tests $NODE_VERSION
+        ;;
+    "all")
+        echo "Running unit tests for all Node versions..."
+        for version in 18 20 22; do
+            run_unit_tests $version
+        done
+        ;;
+    *)
+        echo "Usage: $0 [unit|all] [node_version]"
+        echo "Examples:"
+        echo "  $0 unit 20 # Run unit tests for Node 20"
+        echo "  $0 all # Run unit tests for all Node versions"
+        exit 1
+        ;;
 esac
